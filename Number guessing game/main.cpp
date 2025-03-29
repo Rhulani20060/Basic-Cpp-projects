86c48378767ad047c7cbef9e1c8f2c61c969c4f5
Rhulani20060 <161966003+Rhulani20060@users.noreply.github.com>
2025-03-29 14:02:22 +0200

    Add files via upload
    number guess game 1st commit

---
+++ Number guessing game/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+//____________________________________Function prototypes_______________________________________________//
+
+int randomnumber();
+
+//_______________________________________________________________________________________________________//
+
+int main(){
+
+
+
+    int intguessed = 0;
+    int intrandomresult = 0;
+    bool blnisgame = true;
+    int counter = 1;
+
+
+
+
+        do{
+
+            srand(0);
+
+            cout<<"Guess a number between 1 and 100"<<endl;
+            cin>>intguessed;
+
+            intrandomresult = randomnumber();
+
+            if (intguessed < intrandomresult){
+                cout<<"The number you guessed is lower than the random number, try again!"<<endl;
+            }
+            else if (intguessed > intrandomresult){
+                cout<<"The number you guessed is higher than the random number, try again!"<<endl;
+            }
+            else if (intguessed == intrandomresult){
+                cout<<"Congratulations, you have passed the game in "<< counter<<" moves"<<endl;
+            blnisgame = false;
+            }
+
+            counter++;
+
+        } while (blnisgame);
+
+        return 0;
+}
+
+//_____________________________________function definition_______________________________________________//
+
+ int randomnumber(){
+
+        int intupperb = 100;
+        int intLowerb = 1;
+
+      int range = (intupperb - intLowerb) +1 ;
+      int result = rand() % range + intLowerb;
+      return result;
+ }
